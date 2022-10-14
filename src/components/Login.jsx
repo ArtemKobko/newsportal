ef189a46c8bbf6738b89790a00c49b27df6dd93e
Kobko Artem <arteks505@gmail.com>
2022-10-14 20:21:01 +0200

    styles

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from '../App.modules.scss';
 
 function Login() {
+  const navigate = useNavigate();
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
+  const [valid, setValid] = useState('');
+  // const [visible, setVisible] = useState(false);
 
   function loginValue(event) {
     setLogin(event.target.value);
@@ -10,22 +15,32 @@ function Login() {
   function passwordValue(event) {
     setPassword(event.target.value);
   }
-  function checkUser() {
-    console.log(login, password);
+  function enter() {
+    navigate('/posts');
   }
-
+  function checkUser() {
+    if (login === process.env.REACT_APP_USER && password === process.env.REACT_APP_PASS) {
+      enter();
+    } else {
+      setValid('Login or password is invalid');
+      console.log(styles);
+      // setVisible(true);
+    }
+  }
   return (
-    <div>
+    <div className="login_form">
       <h2>Please sing in</h2>
-      <input type="text" placeholder="login" defaultValue={login} onChange={loginValue} />
+      <h3>{valid}</h3> 
+      <input className={styles.input_log} type="text" placeholder="login" defaultValue={login} onChange={loginValue} />
       <br />
       <input type="password" placeholder="password" defaultValue={password} onChange={passwordValue} />
       <div>
         <input type="checkbox" />
-        Remember me
+        <span>Remember me</span>
+        <a href="/" className="forgot">Forget password?</a>
       </div>
       <div>
-        <button type="submit" onClick={checkUser}>Sing In</button>
+        <button className="btn_singin" type="submit" onClick={checkUser}>Sing In</button>
       </div>
     </div>
   );
