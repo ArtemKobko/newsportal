8ae42a7dd13ee897d3ebb4d450f8884a7ce85a94
Artem Kobko <105234279+ArtemKobko@users.noreply.github.com>
2022-10-27 19:23:16 +0200

    Update src/hooks/useStore.jsx
    Co-authored-by: oleksii.khivrenko <oleksii.khivrenko@gmail.com>

--- src/hooks/useStore.jsx
+++ src/hooks/useStore.jsx
@@ -1,7 +1,7 @@
 import { useReducer } from 'react';
 import authReducer, { initialState } from '../models/auth/reducer';
 
-function useStore(key) {
+function useStore() {
   const isAuth = JSON.parse(localStorage.getItem(key)) || initialState.isAuth;
   const [state, dispatch] = useReducer(authReducer, { ...initialState, isAuth });
 
