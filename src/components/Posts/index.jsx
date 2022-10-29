b76ade5f1c6d86a3612185dabea905c098c390e7
Kobko Artem <arteks505@gmail.com>
2022-10-29 17:58:33 +0200

    posts:add-styles

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import arr from './posts';
+import './posts.scss';
 
 function Posts() {
   return (
     <div>
-      Hello World!
+      <h1 className="mainHeader">Latest Tech News</h1>
+      <div className="posts">
+        {arr.map((e) => (
+          <div key={e.id} className="post">
+            <h2>{e.title}</h2>
+            <p>{e.body}</p>
+            <div className="buttons">
+              <button type="button" className="btnView">View</button>
+              <button type="button" className="btnDelete">Delete</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
